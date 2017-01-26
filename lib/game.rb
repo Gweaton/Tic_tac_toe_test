2eacf4d09cc1835e45d1d85d1454741d30deef02
George Eaton <george_eaton@hotmail.co.uk>
2017-01-26 15:07:29 +0000

    game sets up two players

--- lib/game.rb
+++ lib/game.rb
@@ -1,15 +1,21 @@
 # responsible for the game logic
 class Game
 
-  attr_reader :board, :player1
+  attr_accessor :board, :player1, :player2
 
   def initialize(board)
     @board = board.new
-    @player1 = nil
+    @player1, @player2 = nil
   end
 
-  def add_player1(player)
-    @player1 = player
+  def add_player(player)
+    self.player1 ? self.player2 = player : self.player1 = player unless has_two_players?
+  end
+
+  private
+
+  def has_two_players?
+    (player1 != nil && player2 != nil)
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,6 +5,7 @@ describe Game do
   let(:board) { double :board }
   subject(:game) { described_class.new(board) }
   let(:player1) { double :player1 }
+  let(:player2) { double :player2}
 
   before do
     allow(board).to receive(:new).and_return board
@@ -15,9 +16,18 @@ describe Game do
     expect(game.board).to eq board
   end
 
-  it "can add a player" do
-    game.add_player1(player1)
-    expect(game.player1).to eq player1
+  context "players" do
+    before do
+      game.add_player(player1)
+    end
+    it "can add a player" do
+      expect(game.player1).to eq player1
+    end
+
+    it "can add a second player" do
+      game.add_player(player2)
+      expect(game.player2).to eq player2
+    end
   end
 
 end
