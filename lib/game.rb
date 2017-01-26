4eee39fda7f260735b8b3aa7da7122114fe15d8f
George Eaton <george_eaton@hotmail.co.uk>
2017-01-26 14:57:24 +0000

    creates add_player1 method

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,15 @@
+# responsible for the game logic
 class Game
 
-  attr_reader :board
+  attr_reader :board, :player1
 
   def initialize(board)
     @board = board.new
+    @player1 = nil
+  end
+
+  def add_player1(player)
+    @player1 = player
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,6 +4,7 @@ describe Game do
 
   let(:board) { double :board }
   subject(:game) { described_class.new(board) }
+  let(:player1) { double :player1 }
 
   before do
     allow(board).to receive(:new).and_return board
@@ -12,6 +13,11 @@ describe Game do
 
   it "should be initialized with a board" do
     expect(game.board).to eq board
+  end
+
+  it "can add a player" do
+    game.add_player1(player1)
+    expect(game.player1).to eq player1
   end
 
 end
